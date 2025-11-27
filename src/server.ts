94e994caf985ca6fdd8ea60c8a586224afb24ee8
Babla Dey <babladey275@gmail.com>
2025-11-27 21:24:37 +0600

    added global error and SIGTERM handlers

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,66 @@
 import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
-require('dotenv').config();
+require("dotenv").config();
 
 let server: Server;
 
 const startServer = async () => {
   try {
-    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wf6wg.mongodb.net/tour-management-backend?appName=Cluster0`)
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wf6wg.mongodb.net/tour-management-backend?appName=Cluster0`
+    );
 
-    console.log("Connected to DB")
+    console.log("Connected to DB");
 
-    server = app.listen(5000, ()=>{
-      console.log("Server is listening to port 5000")
-    })
-  } catch (error){
-    console.log(error)
+    server = app.listen(5000, () => {
+      console.log("Server is listening to port 5000");
+    });
+  } catch (error) {
+    console.log(error);
   }
 };
 
-startServer()
+startServer();
 
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection detected... Server shutting down...", err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception detected... Server shutting down..", err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on("SIGTERM", () => {
+  console.log("Sigterm signal received... Server shutting down..");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+//unhandled rejection error
+// Promise.reject(new Error("I forgot to catch this promise"));
+
+//uncaught exception
+// throw new Error("I forgot to handle this local error")
