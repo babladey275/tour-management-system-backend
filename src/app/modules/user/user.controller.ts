822bcae6839a1f5e7737e9b98d2c0ab1d2fee257
Babla Dey <babladey275@gmail.com>
2025-11-29 10:05:30 +0600

    added catchAsync

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,22 +1,47 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { UserServices } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+// const createUser = async (req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     const user = await UserServices.createUser(req.body);
+
+//     res.status(httpStatus.CREATED).json({
+//       message: "User Created Successfully",
+//       user,
+//     });
+//   } catch (err: any) {
+//     next(err);
+//   }
+// };
+
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const user = await UserServices.createUser(req.body);
 
     res.status(httpStatus.CREATED).json({
       message: "User Created Successfully",
       user,
     });
-  } catch (err: any) {
-    next(err);
   }
-};
+);
+
+const getAllUsers = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const users = await UserServices.getAllUsers();
+
+    res.status(httpStatus.OK).json({
+      success: true,
+      message: "All Users Retrieve Successfully",
+      data: users,
+    });
+  }
+);
 
 export const UserControllers = {
   createUser,
+  getAllUsers,
 };
 
 //route matching -> controller -> service -> model -> DB
