924f53fadd5549771fc94f2be2f9b16b9bdb224f
Babla Dey <babladey275@gmail.com>
2025-12-11 12:22:27 +0600

    added passport local strategy for credential login

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -8,31 +8,66 @@ import { setAuthCookie } from "../../utils/setCookie";
 import { JwtPayload } from "jsonwebtoken";
 import { createUserTokens } from "../../utils/userTokens";
 import { envVars } from "../../config/env";
+import passport from "passport";
 
 const credentialsLogin = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const loginInfo = await AuthServices.credentialsLogin(req.body);
+    passport.authenticate("local", async (err: any, user: any, info: any) => {
+      if (err) {
+        // return next(err);
+        return next(new AppError(401, err));
+      }
 
-    // res.cookie("accessToken", loginInfo.accessToken, {
-    //   httpOnly: true,
-    //   secure: false,
-    // });
+      if (!user) {
+        return next(new AppError(401, info.message));
+      }
 
-    // res.cookie("refreshToken", loginInfo.refreshToken, {
-    //   httpOnly: true,
-    //   secure: false,
-    // });
+      const userTokens = createUserTokens(user);
 
-    setAuthCookie(res, loginInfo);
+      // delete user.toObject.password;
 
-    sendResponse(res, {
-      success: true,
-      statusCode: httpsStatus.OK,
-      message: "User Logged In Successfully",
-      data: loginInfo,
-    });
+      const { password, ...rest } = user.toObject();
+
+      setAuthCookie(res, userTokens);
+
+      sendResponse(res, {
+        success: true,
+        statusCode: httpsStatus.OK,
+        message: "User Logged In Successfully",
+        data: {
+          accessToken: userTokens.accessToken,
+          refreshToken: userTokens.refreshToken,
+          user: rest,
+        },
+      });
+    })(req, res, next);
   }
 );
+
+// const credentialsLogin = catchAsync(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     const loginInfo = await AuthServices.credentialsLogin(req.body);
+
+//     // res.cookie("accessToken", loginInfo.accessToken, {
+//     //   httpOnly: true,
+//     //   secure: false,
+//     // });
+
+//     // res.cookie("refreshToken", loginInfo.refreshToken, {
+//     //   httpOnly: true,
+//     //   secure: false,
+//     // });
+
+//     setAuthCookie(res, loginInfo);
+
+//     sendResponse(res, {
+//       success: true,
+//       statusCode: httpsStatus.OK,
+//       message: "User Logged In Successfully",
+//       data: loginInfo,
+//     });
+//   }
+// );
 
 const getNewAccessToken = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -106,9 +141,9 @@ const resetPassword = catchAsync(
 
 const googleCallback = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    let redirectTo = req.query.state ? req.query.state as string : "";
+    let redirectTo = req.query.state ? (req.query.state as string) : "";
 
-    if(redirectTo.startsWith("/")){
+    if (redirectTo.startsWith("/")) {
       redirectTo = redirectTo.slice(1);
     }
 
