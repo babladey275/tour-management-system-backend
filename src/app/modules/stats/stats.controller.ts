696c8b00083fd367cbbefb58f71ea0fb2524d5e7
Babla Dey <babladey275@gmail.com>
2026-01-01 21:55:27 +0600

    added get user API with role based access for admin stats

---
+++ src/app/modules/stats/stats.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+import { StatsService } from "./stats.service";
+import { sendResponse } from "../../utils/sendResponse";
+
+const getUserStats = catchAsync(async (req: Request, res: Response) => {
+  const stats = await StatsService.getUserStats();
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "User stats fetched successfully",
+    data: stats,
+  });
+});
+
+export const StatsController = {
+  getUserStats,
+};
---
+++ src/app/modules/stats/stats.service.ts
@@ -0,0 +1,68 @@
+import { AccountStatus } from "../user/user.interface";
+import { User } from "../user/user.model";
+
+const now = new Date();
+const sevenDaysAgo = new Date(now).setDate(now.getDate() - 7);
+const thirtyDaysAgo = new Date(now).setDate(now.getDate() - 30);
+
+const getUserStats = async () => {
+  const totalUsersPromise = User.countDocuments();
+
+  const totalActiveUsersPromise = User.countDocuments({
+    status: AccountStatus.ACTIVE,
+  });
+  const totalInActiveUsersPromise = User.countDocuments({
+    status: AccountStatus.INACTIVE,
+  });
+  const totalBlockedUsersPromise = User.countDocuments({
+    status: AccountStatus.BLOCKED,
+  });
+
+  const newUsersInLast7DaysPromise = User.countDocuments({
+    createdAt: { $gte: sevenDaysAgo },
+  });
+  const newUsersInLast30DaysPromise = User.countDocuments({
+    createdAt: { $gte: thirtyDaysAgo },
+  });
+
+  const usersByRolePromise = User.aggregate([
+    //stage -1 : Grouping users by role and count total users in each role
+    {
+      $group: {
+        _id: "$role",
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+
+  const [
+    totalUsers,
+    totalActiveUsers,
+    totalInActiveUsers,
+    totalBlockedUsers,
+    newUsersInLast7Days,
+    newUsersInLast30Days,
+    usersByRole,
+  ] = await Promise.all([
+    totalUsersPromise,
+    totalActiveUsersPromise,
+    totalInActiveUsersPromise,
+    totalBlockedUsersPromise,
+    newUsersInLast7DaysPromise,
+    newUsersInLast30DaysPromise,
+    usersByRolePromise,
+  ]);
+  return {
+    totalUsers,
+    totalActiveUsers,
+    totalInActiveUsers,
+    totalBlockedUsers,
+    newUsersInLast7Days,
+    newUsersInLast30Days,
+    usersByRole,
+  };
+};
+
+export const StatsService = {
+  getUserStats,
+};
