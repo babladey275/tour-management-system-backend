31c9a331a20a785dfba36b0cf5427f9ea0a4df19
Babla Dey <babladey275@gmail.com>
2026-01-01 19:05:01 +0600

    refactor user APIs and set up frontend CORS

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -20,7 +20,7 @@ router.get(
   UserControllers.getAllUsers
 );
 router.get("/me", checkAuth(...Object.values(Role)), UserControllers.getMe)
-router.get("/:id", checkAuth(...Object.values(Role)), UserControllers.getSingleUser)
+router.get("/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), UserControllers.getSingleUser)
 router.patch(
   "/:id",
   checkAuth(...Object.values(Role)),
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -43,11 +43,22 @@ const updateUser = async (
   payload: Partial<IUser>,
   decodedToken: JwtPayload
 ) => {
+  if(decodedToken.role === Role.USER || decodedToken.role === Role.GUIDE){
+    if(userId !== decodedToken.userId){
+      throw new AppError(401, "You are not authorized")
+    }
+  }
+
   const isUserExist = await User.findById(userId);
 
   if (!isUserExist) {
     throw new AppError(httpStatus.NOT_FOUND, "User Not Found");
   }
+
+  if(decodedToken.role === Role.ADMIN && isUserExist.role === Role.SUPER_ADMIN){
+    throw new AppError(401, "You are not authorized.")
+  }
+
   /**
    * email - can not update
    * name, phone, password, address
@@ -62,22 +73,15 @@ const updateUser = async (
       throw new AppError(httpStatus.FORBIDDEN, "You are not authorized");
     }
 
-    if (payload.role === Role.SUPER_ADMIN && decodedToken.role === Role.ADMIN) {
-      throw new AppError(httpStatus.FORBIDDEN, "You are not authorized");
-    }
+    // if (payload.role === Role.SUPER_ADMIN && decodedToken.role === Role.ADMIN) {
+    //   throw new AppError(httpStatus.FORBIDDEN, "You are not authorized");
+    // }
   }
 
   if (payload.status || payload.isDeleted || payload.isVerified) {
     if (decodedToken.role === Role.USER || decodedToken.role === Role.GUIDE) {
       throw new AppError(httpStatus.FORBIDDEN, "You are not authorized");
     }
-  }
-
-  if (payload.password) {
-    payload.password = await bcryptjs.hash(
-      payload.password,
-      Number(envVars.BCRYPT_SALT_ROUND)
-    );
   }
 
   const newUpdatedUser = await User.findByIdAndUpdate(userId, payload, {
